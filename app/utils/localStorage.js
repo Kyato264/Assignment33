c0ab8ba34b758b724b7c2710108a25bf470f7328
Kyato264 <158293638+Kyato264@users.noreply.github.com>
2025-04-14 17:27:00 -0400

    Tasks Update
    LocalStorage was not updating tasks

--- app/utils/localStorage.js
+++ app/utils/localStorage.js
@@ -3,11 +3,15 @@ import predefinedTasks from "../data/predefinedTasks";
 
 export function getTasksFromStorage() {
     const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    console.log("Received tasks from storagee:", tasks);
+    console.log(Array.isArray(tasks) ? tasks : []);
     return Array.isArray(tasks) ? tasks : [];
 }
 
 export function initializeTasksInStorage() {
+    console.log("InitializeTasksInStorage has been called");
     if (typeof window !== "undefined" && !localStorage.getItem("tasks")) {
+        console.log("Initializing tasks in local storage");
         localStorage.setItem("tasks", JSON.stringify(predefinedTasks));
     }
 }
