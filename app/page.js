d2b439054aea84031c407c2a06b52a2eecf55494
Kyato264 <158293638+Kyato264@users.noreply.github.com>
2025-04-15 17:32:34 -0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 // app/page.js
 "use client";
 import TaskList from "./screens/TaskList";
-//i forgot about this page while testing
+//i forgot about this page while working and by the time i realizied i already had the page mostly done, ive attempted to move the page but alot of routes would mess up so i just left it here
 export default function Home() {
   return <TaskList />;
 }
